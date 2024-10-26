060309c6d11297c1d5551d2f788893a50985909f
kweku-xvi <nksarps@gmail.com>
2024-10-26 11:01:15 +0000

    added routes for users

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kweku-xvi/todo/api/v1/controllers"
+	"github.com/kweku-xvi/todo/api/v1/middleware"
 	"github.com/kweku-xvi/todo/internal/database"
 )
 
@@ -20,6 +21,13 @@ func main() {
 		v1.GET("/tasks/:id", controllers.GetTaskByID)
 		v1.PUT("/tasks/:id", controllers.UpdateTask)
 		v1.DELETE("/tasks/:id", controllers.DeleteTask)
+	}
+
+	auth := r.Group("/api/v1/auth")
+	{
+		auth.POST("/signup", controllers.SignUp)
+		auth.POST("/login", controllers.SignIn)
+		auth.GET("/user/profile", middleware.CheckAuth, controllers.GetUserProfile)
 	}
 
 	r.Run() // listen and serve on 0.0.0.0:8080
